e1d5ee2254296b69532b37e5e375df25bb01730b
sidee <said.issack@student.moringaschool.com>
2023-09-27 12:26:27 +0300

    completed

--- server/app.py
+++ server/app.py
@@ -14,14 +14,43 @@ app.json.compact = True
 migrate = Migrate(app, db)
 db.init_app(app)
 
-api = Api(app)
+    api = Api(app)
 
 class Plants(Resource):
     pass
+    def get(self):
+        plants_dict = [p.to_dict() for p in Plant.query.all()]
+
+        response = make_response(jsonify(plants_dict), 200)
+
+        return response
+
+    def post(self):
+        name = request.form.get('name')
+        image = request.form.get('image')
+        price = request.form.get('price')
+
+        new_plant = Plant(name=name, image=image, price=price)
+
+        db.session.add(new_plant)
+        db.session.commit()
+
+        response = make_response(jsonify(new_plant), 201)
+
+        return response
+
+api.add_resource(Plants, '/plants')    
 
 class PlantByID(Resource):
     pass
-        
+    def get(self, id):
+        plant_dict = Plant.query.filter_by(id=id).first().to_dict()
+
+        response = make_response(jsonify(plant_dict), 200)
+
+        return response
+
+api.add_resource(PlantByID, '/plants/<int:id>')    
+
 
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
